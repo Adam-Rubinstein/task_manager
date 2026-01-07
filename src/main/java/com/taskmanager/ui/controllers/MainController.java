7030f11b391a62a73fde78ddff143dedc6f7a576
Adam-Rubinstein <adam-rubinstein@mail.ru>
2026-01-08 02:38:12 +0300

    Auto-commit: 2026-01-08 02:38:12

--- src/main/java/com/taskmanager/ui/controllers/MainController.java
+++ src/main/java/com/taskmanager/ui/controllers/MainController.java
@@ -480,16 +480,11 @@ public class MainController {
             ScrollPane scrollPane = new ScrollPane(mainVBox);
             scrollPane.setFitToWidth(true);
             javafx.scene.Scene scene = new javafx.scene.Scene(scrollPane);
-
-            ScrollPane scrollPane = new ScrollPane(mainVBox);
-            scrollPane.setFitToWidth(true);
-
+            
             // ПРИМЕНЯЕМ ТЕМУ К SCROLL PANE
             if (isDarkTheme) {
                 scrollPane.setStyle("-fx-background-color: #1e1e1e; -fx-control-inner-background: #2b2b2b;");
             }
-
-            javafx.scene.Scene scene = new javafx.scene.Scene(scrollPane);
 
             // ПРИМЕНЯЕМ ТЕМУ К САМОЙ СЦЕНЕ
             if (isDarkTheme) {
